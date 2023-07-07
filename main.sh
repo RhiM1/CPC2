542469b6c488aba6a873fb5e1867538012a86825
RhiM1 <rmogridg1@sheffield.ac.uk>
2023-07-07 12:13:47 +0100

    Clean version

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python MAIN_predict_intel_correctness_feats.py --feats_model WhisperFull --model LSTM --exp_id 1_layer --batch_size 2 --layer 11 --n_epochs 10 --skip_wandb --weight_decay 0.001 --debug
-python MAIN_predict_intel_correctness_feats.py --feats_model WhisperFull --model LSTM_layers --exp_id all_layer --batch_size 2 --layer -1 --n_epochs 10 --skip_wandb --weight_decay 0.001 --debug
+CUDA_VISIBLE_DEVICES=0 python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model LSTM --N 1 --exp_id test --batch_size 2 --layer 11 --n_epochs 1 --debug
+#CUDA_VISIBLE_DEVICES=0 python MAIN_predict_intel_correctness_feats.py --feats WhisperFull --model LSTM --N 1 --exp_id 001 --batch_size 2 --layer 11 --n_epochs 20
+
